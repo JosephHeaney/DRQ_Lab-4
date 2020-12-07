1078e0a1a5c953c274f15ba9e14fde7d81a94dbd
Joseph Heaney <G00369134@gmit.ie>
2020-12-07 16:09:30 +0000

    Lab Complete

---
+++ src/components/read.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Movies } from './movies';
+import axios from 'axios';
+
+export class Read extends React.Component {
+
+    state = {
+        movies: []
+    };
+
+    componentDidMount() {
+        axios.get('https://jsonblob.com/api/jsonblob/520c3b5e-0312-11eb-a6af-cbf00d776032')
+            .then((response) => {
+                this.setState({ movies: response.data.Search })
+            })
+            .catch((error) => { console.log(error) }
+            );
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>This is the Read Component.</h1>
+                <Movies movies={this.state.movies}></Movies>
+            </div>
+        )
+    }
+}
